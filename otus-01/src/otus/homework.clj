8fc56f4704a42094f06314d6a4123cae4aab81d3
Mazo Vasiliy <mazo@protonmail.ch>
2023-05-08 00:41:24 +0300

    original state

--- otus-01/src/otus/homework.clj
+++ otus-01/src/otus/homework.clj
@@ -1,9 +1,4 @@
 (ns otus.homework)
 
 
-(defn solution
-  "Add your solution as fuction body here"
-  []
-  (-> (/ (+ 5 4 (- 2 (- 3 (+ 6 4/5))))
-         (* 3 (- 6 2) (- 2 7)))
-      double))
+(defn solution "Add your solution as fuction body here" [])
