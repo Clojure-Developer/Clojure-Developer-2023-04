20eb867ccc3f8949297457463562843dd811be04
Dmitry Pavlov <mitrickx@mail.ru>
2023-05-09 10:20:44 +0300

    Homework2: is-palindrome

--- otus-02/src/otus_02/homework/palindrome.clj
+++ otus-02/src/otus_02/homework/palindrome.clj
@@ -1,4 +1,22 @@
-(ns otus-02.homework.palindrome)
+(ns otus-02.homework.palindrome
+  (:require [clojure.string :as str]))
+
+(defn is-palindrome [test-string]
+  (let [original (str/lower-case
+                   (str/replace test-string #",|!|\?|\.|\s" ""))
+        reversed (str/reverse original)]
+    (= original reversed)))
 
 
-(defn is-palindrome [test-string])
+
+
+
+
+
+
+
+
+
+
+
+
