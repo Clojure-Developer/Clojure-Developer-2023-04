bd0335d5cb4b651a898a4c688a7bf273d95d1e2e
leosсhulmann <leo.schulmannn@gmail.com>
2023-05-09 23:02:58 +0300

    solution

--- otus-02/src/otus_02/homework/pangram.clj
+++ otus-02/src/otus_02/homework/pangram.clj
@@ -1,4 +1,8 @@
-(ns otus-02.homework.pangram)
+(ns otus-02.homework.pangram
+  (:require [clojure.string :as str]))
 
-
-(defn is-pangram [test-string])
+(defn is-pangram [test-string]
+  (let [alphabet (vec "abcdefghijklmnopqrstuvwxyz")]
+    (= alphabet
+       (sort (distinct
+               (str/lower-case (str/replace test-string #"\W" "")))))))
