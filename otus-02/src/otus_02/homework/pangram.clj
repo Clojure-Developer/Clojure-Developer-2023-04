b0db8fff6175093a950ed2941aa4c7ecf89cb09f
sergevas <brighterr@gmail.com>
2023-05-19 12:02:03 +0300

    Added HW_02 solution

--- otus-02/src/otus_02/homework/pangram.clj
+++ otus-02/src/otus_02/homework/pangram.clj
@@ -1,4 +1,12 @@
-(ns otus-02.homework.pangram)
+(ns otus-02.homework.pangram
+  (:require [clojure.string :as string]))
 
+(defn generate-abc []
+  (map char (range (int \a) (int \z))))
 
-(defn is-pangram [test-string])
+(defn trim-nonalph-and-lcase [sentence]
+  (string/lower-case (string/replace sentence #"[^a-zA-Z]" "")))
+
+(defn is-pangram [test-string]
+  (every? (set (trim-nonalph-and-lcase test-string))
+          (generate-abc)))
