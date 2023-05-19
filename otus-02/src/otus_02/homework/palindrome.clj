18b9782b4be322723a4be52910e21169698ffa23
sergevas <brighterr@gmail.com>
2023-05-19 12:02:03 +0300

    Added HW_02 solution

--- otus-02/src/otus_02/homework/palindrome.clj
+++ otus-02/src/otus_02/homework/palindrome.clj
@@ -1,4 +1,7 @@
-(ns otus-02.homework.palindrome)
+(ns otus-02.homework.palindrome
+  (:require [clojure.string :as string]))
 
 
-(defn is-palindrome [test-string])
+(defn is-palindrome [test-string]
+  (let [alphanum-only-string (string/lower-case (string/replace test-string #"\W" ""))]
+    (= alphanum-only-string (string/reverse alphanum-only-string))))
