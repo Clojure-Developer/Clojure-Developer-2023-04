5961797160fad99af40a2e2643ad1ad81ead0ef5
Dmitry Pavlov <mitrickx@mail.ru>
2023-05-22 10:09:12 +0300

    Homework3: fix scramblies to pass new tests

--- otus-04/src/otus_04/homework/scramblies.clj
+++ otus-04/src/otus_04/homework/scramblies.clj
@@ -3,9 +3,27 @@
 ;; Оригинальная задача:
 ;; https://www.codewars.com/kata/55c04b4cc56a697bb0000048
 
+(defn count-letters [word]
+  (reduce
+    (fn [stat ch]
+      (let [cnt (get stat ch 0)]
+        (assoc stat ch (inc cnt))))
+    {}
+    word))
+
+(defn de-count-letters [init-stat word]
+  (reduce
+    (fn [stat ch]
+      (let [cnt (get stat ch 0)]
+        (assoc stat ch (dec cnt))))
+    init-stat
+    word))
+
 (defn scramble?
   "Функция возвращает true, если из букв в строке letters
   можно составить слово word."
   [letters word]
-  (let [letters-set (set letters)]
-    (every? (partial contains? letters-set) word)))
+  (let [stat-letters (count-letters letters)
+        result-stat  (de-count-letters stat-letters word)
+        with-neg-values (filter (fn [[_ v]] (neg? v)) result-stat)]
+    (zero? (count with-neg-values))))
