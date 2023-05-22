622152aa1704ef5b1b6ec25c60b85ffb7eddcaa7
bondiano <batbondik0@gmail.com>
2023-05-22 21:37:49 +0600

    finish first homework

--- otus-01/src/otus/homework.clj
+++ otus-01/src/otus/homework.clj
@@ -1,5 +1,9 @@
 (ns otus.homework)
 
-
-(defn solution "Add your solution as fuction body here" [])
-
+(defn solution []
+  (double
+    (/ (+ 5 4
+          (- 2 (- 3 (+ 6 (/ 4 5)))))
+       (* 3
+          (- 6 2)
+          (- 2 7)))))
