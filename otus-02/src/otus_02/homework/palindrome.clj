3f17c4d1953f12c0bd1fae1b2e6a836fa38bcd13
bondiano <batbondik0@gmail.com>
2023-05-24 00:02:16 +0600

    add solution for palindrom and panagram

--- otus-02/src/otus_02/homework/palindrome.clj
+++ otus-02/src/otus_02/homework/palindrome.clj
@@ -1,4 +1,12 @@
-(ns otus-02.homework.palindrome)
+(ns otus-02.homework.palindrome
+  (:require [clojure.string :as str]))
 
+(defn normalize [test-string]
+  (-> test-string
+      str/lower-case
+      str/trim
+      (str/replace #"[^a-z0-9]" "")))
 
-(defn is-palindrome [test-string])
+(defn is-palindrome [test-string]
+  (let [string (normalize test-string)]
+    (= string (str/reverse string))))
--- otus-02/src/otus_02/homework/pangram.clj
+++ otus-02/src/otus_02/homework/pangram.clj
@@ -1,4 +1,16 @@
-(ns otus-02.homework.pangram)
+(ns otus-02.homework.pangram
+  (:require [clojure.string :as str]))
 
+(def alphabet-set (->> (range (int \a) (inc (int \z)))
+                       (map char)
+                       set))
 
-(defn is-pangram [test-string])
+(defn string->normalized-set [test-string]
+  (-> test-string
+      str/lower-case
+      str/trim
+      (str/replace #"[^a-z0-9]" "")
+      set))
+
+(defn is-pangram [test-string]
+  (= alphabet-set (string->normalized-set test-string)))
