6a839fb99858b2cb6212bdcbaa41a41c8cc21a99
Dmitry Pavlov <mitrickx@mail.ru>
2023-05-24 09:44:04 +0300

    Homework3: crossword WIP

--- otus-04/src/otus_04/homework/crossword_puzzle.clj
+++ otus-04/src/otus_04/homework/crossword_puzzle.clj
@@ -14,7 +14,7 @@
   "Вставляем в вектор символов символы chars начиная с позиции i,
   заменяя каждый символ вектора на очередной символ из chars"
   [input i chars]
-  (concat (take i input) chars (drop (+ i (count chars)) input)))
+  (vec (concat (take i input) chars (drop (+ i (count chars)) input))))
 
 (defn update-horizontal
   "Обновляем в поле горизонтальную линию словом word, начиная с позиции row col
@@ -46,16 +46,14 @@
         words (str/split (last lines) #";")]
     {:board board :words words}))
 
-(defn search [input ch]
-  (let [pos (.indexOf input ch)]
-    (if (= pos -1) nil pos)))
+(defn search [input ch] (.indexOf input ch))
 (defn find-free-cell
   "Ищем первую свободную ячейку '-'"
   [board]
   (let [n (count board)]
     (loop [row 0
            result nil]
-      (if (or (not (nil? result)) (>= row n))
+      (if (or (some? result) (>= row n))
         result
         (let [board-row (get board row)
               col (search board-row \-)
@@ -99,9 +97,9 @@
     (loop [new-col col]
       (if (valid-position? board row new-col)
         (let [cell (get-in board [row new-col])]
-          (if (= cell \-)
-            (recur (dec new-col))
-            (inc new-col)))
+          (if (= cell \+)
+            (inc new-col)
+            (recur (dec new-col))))
         0))
     nil))
 
@@ -113,9 +111,9 @@
    (loop [new-row row]
      (if (valid-position? board new-row col)
        (let [cell (get-in board [new-row col])]
-         (if (= cell \-)
-           (recur (dec new-row))
-           (inc new-row)))
+         (if (= cell \+)
+           (inc new-row)
+           (recur (dec new-row))))
        0))
    nil))
 
@@ -154,27 +152,38 @@ LONDON;DELHI;ICELAND;ANKARA")
   (cond
     (horizontal? board row col)
     (let [start (get-horizontal-start board row col)]
-      {:line (get-horizontal board row start) :type 'horizontal})
+      {:line (get-horizontal board row start) :type 'horizontal :row row :col start})
 
     (vertical? board row col)
     (let [start (get-vertical-start board row col)]
-      {:line (get-vertical board start col) :type 'vertical})
+      {:line (get-vertical board start col) :type 'vertical :row start :col col})
 
     :else nil))
 
-;; Идем либо в горизноталь либо в вертикаль - в любом случае получаем список слов которые "подходят" и оставшиеся
-;; Возварщаем так же "тип" (вертикаль или горизонталь)
-;; Если тип не подходит - возвращаем подходящие как пустой список
-;; Потом (в другой функции) перебираем подходящие, вставляя в борду и рекурсиво запускаем солвер на U от оставшиеся подходящие + оставшиеся
-(defn make-one-step
+(defn calc-next-move
   [board words]
   (let [[row col]         (find-free-cell board)
         blank-line-result (get-blank-line board row col)]
     (if (some? blank-line-result)
-      (let [{line :line type :type} blank-line-result
+      (let [{line :line type :type row :row col :col} blank-line-result
             mask                    (make-mask line)
             [matches other]         (split-by-mask words mask)]
-        {:matches matches :other other :type type})
+        {:matches matches :other other :type type :row row :col col})
+      nil)))
+
+(defn make-next-move
+  [board words]
+  (let [state (calc-next-move board words)
+        {[first-match & rest-matches] :matches
+         type :type
+         row :row
+         col :col} state]
+    (if (some? first-match)
+      (let [new-board
+            (if (= type 'horizontal)
+              (update-horizontal board row col first-match)
+              (update-vertical board row col first-match))]
+        (merge state {:board new-board :matches rest-matches}))
       nil)))
 
 (defn solve
