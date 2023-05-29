150251b91dffbbac7d9e554a242afcc620594c1c
Dmitry Pavlov <mitrickx@mail.ru>
2023-05-29 19:04:28 +0300

    Homework3: fix another review points

--- otus-04/src/otus_04/homework/crossword_puzzle.clj
+++ otus-04/src/otus_04/homework/crossword_puzzle.clj
@@ -33,6 +33,7 @@
   (let [board-row (get board row)
         new-board-row (assoc board-row col ch)]
     (assoc board row new-board-row)))
+
 (defn add-word-vertically
   "Добавляем в поле вертикально слово word, начиная с позиции row col
   Поле - вектор векторов"
@@ -67,7 +68,7 @@
               (let [col (search board-row \-)
                     ok? (>= col 0)]
                 {:row row :col col :ok? ok?}))
-         (iterate inc 0))
+            (iterate inc 0))
        (filter #(:ok? %))
        first))
 
@@ -77,61 +78,56 @@
   (let [n (count board)]
     (and (< row n) (< col n) (>= row 0) (>= col 0))))
 
+(defn on-the-line?
+  [board row col get-nearest-cell-coordinates]
+  (and
+    (valid-position? board row col)
+    (let [cell (get-in board [row col])
+          [r c] (get-nearest-cell-coordinates row col)
+          nearest-cell (get-in board [r c])]
+      (and (= cell \-) (= nearest-cell \-)))))
+
 (defn horizontal?
-  "Находимся ли мы на свободной горизинтальной линии кросворда,
-  предполагая row и col указывают на свободную ячейка (то есть '-')"
+  "Находимся ли мы на свободной горизонтальной линии кроссворда,
+  предполагая row и col указывают на свободную ячейку (то есть '-')"
   [board row col]
-  (let [n (count board)]
-    (if (valid-position? board row col)
-      (let [cell         (get-in board [row col])
-            nearest-col  (if (< col (dec n)) (inc col) (dec col))
-            nearest-cell (get-in board [row nearest-col])]
-        (and (= cell \-) (= nearest-cell \-)))
-      false)))
+  (on-the-line? board row col
+                (fn [r c] [r (if (< c (dec (count board))) (inc c) (dec c))])))
 
 (defn vertical?
-  "Находимся ли мы на вертикальной линии кросворда,
-  предполагая row и col указаывают на свободную ячейку (то есть '-')"
+  "Находимся ли мы на вертикальной линии кроссворда,
+  предполагая row и col указывают на свободную ячейку (то есть '-')"
   [board row col]
-  (let [n (count board)]
-    (if (valid-position? board row col)
-      (let [cell              (get-in board [row col])
-            nearest-row       (if (< row (dec n)) (inc row) (dec row))
-            nearest-cell      (get-in board [nearest-row col])]
-        (and (= cell \-) (= nearest-cell \-)))
-      false)))
+  (on-the-line? board row col
+                (fn [r c] [(if (< r (dec (count board))) (inc r) (dec r)) c])))
+(defn get-start
+  [board row col on-next on-final]
+  (when (and
+          (valid-position? board row col)
+          (= (get-in board [row col]) \-))
+    (loop [[new-row new-col] [row col]]
+      (if (valid-position? board new-row new-col)
+        (let [cell (get-in board [new-row new-col])]
+          (if (= cell \+)
+            (on-final new-row new-col)
+            (recur (on-next new-row new-col))))
+        0))))
 
 (defn get-horizontal-start
   "Находим начало горизонтальной линии, на которую указывает row col,
   предполагая, что row и col указывают на свободную ячейку"
   [board row col]
-  (if (and
-        (valid-position? board row col)
-        (= (get-in board [row col]) \-))
-    (loop [new-col col]
-      (if (valid-position? board row new-col)
-        (let [cell (get-in board [row new-col])]
-          (if (= cell \+)
-            (inc new-col)
-            (recur (dec new-col))))
-        0))
-    nil))
+  (get-start board row col
+             (fn [row col] [row (dec col)])
+             (fn [_ col] (inc col))))
 
 (defn get-vertical-start
   "Находим начало вертикальной линии, на которую указывает row col,
   предполагая, что row и col указывают на свободную ячейку"
   [board row col]
-  (if (and
-       (valid-position? board row col)
-       (= (get-in board [row col]) \-))
-   (loop [new-row row]
-     (if (valid-position? board new-row col)
-       (let [cell (get-in board [new-row col])]
-         (if (= cell \+)
-           (inc new-row)
-           (recur (dec new-row))))
-       0))
-   nil))
+  (get-start board row col
+             (fn [row col] [(dec row) col])
+             (fn [row _] (inc row))))
 
 (defn get-horizontal
   "Вычитываем текущую горизонтальную линию кроссворда (необязательно полностью свободную) в строку,
@@ -170,15 +166,13 @@
   "Просчитываем состояние следующего шага в дереве решения
   Вернет список подходящих слов, список оставшихся слов, тип ориентации слова и координаты ячейки для вставки слова"
   [board words]
-  (let [{row :row col :col ok? :ok?} (find-free-cell board)]
+  (let [{:keys [row col ok?]} (find-free-cell board)]
     (if ok?
-      (let [blank-line-result (get-free-line board row col)]
-        (if (some? blank-line-result)
-          (let [{line :line type :type row :row col :col} blank-line-result
-                mask                    (make-mask line)
-                [matches other]         (split-by-mask words mask)]
-            {:matches matches :other other :type type :row row :col col})
-          nil))
+      (when-let [blank-line-result (get-free-line board row col)]
+        (let [{line :line type :type row :row col :col} blank-line-result
+              mask (make-mask line)
+              [matches other] (split-by-mask words mask)]
+          {:matches matches :other other :type type :row row :col col}))
       nil)))
 
 (defn make-one-move
@@ -194,18 +188,13 @@
 (defn do-solve-next-move
   "Находим решение для текущей доски и просчитанного состояния для следующего шага"
   [board next-move-state]
-  (let [{matches :matches
-         other :other
-         type :type
-         row :row
-         col :col} next-move-state]
-    (loop [first-match  (first matches)
-           rest-matches (rest matches)
-           other-words  other]
+  (let [{:keys [matches other type row col]} next-move-state]
+    (loop [[first-match & rest-matches] matches
+           other-words other]
       (if (some? first-match)
-        (let [rest-words   (concat rest-matches other-words)
+        (let [rest-words (concat rest-matches other-words)
               ;; обновили доску подходящим словом в нужном месте по нужному направлению
-              new-board    (make-one-move board first-match type row col)
+              new-board (make-one-move board first-match type row col)
               ;; теперь рекурсивно пробуем решить доску на всех оставшихся словах
               solve-result (do-solve new-board rest-words)]
           (if (:ok? solve-result)
@@ -213,8 +202,7 @@
             solve-result
             ;; иначе пробуем следующее подходящее слово
             (recur
-              (first rest-matches)
-              (rest rest-matches)
+              rest-matches
               ;; задвинув слово на котором только что попробовали вконец списка
               (concat other-words [first-match]))))
         {:board board :ok? false}))))
@@ -223,7 +211,7 @@
   "Решаем кроссворд для текущей доски и общего списка слов, которым мы располагаем на данный момент"
   [board words]
   (if (pos? (count words))
-    (let [next-move-state    (calc-next-move board words)
+    (let [next-move-state (calc-next-move board words)
           {matches :matches} next-move-state]
       (if (some? matches)
         ;; нашли подходящие слова, которые могут привести нас к решению -
@@ -257,6 +245,6 @@
   сверху-вниз или слева-направо."
   [input]
   (let [{board :board words :words} (parse-input input)
-        solve-result                (do-solve board words)
-        {ok? :ok? board :board}      solve-result]
+        solve-result (do-solve board words)
+        {ok? :ok? board :board} solve-result]
     (if ok? (str->board board) "")))
