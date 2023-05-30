92ff4cad4e7e7c762a63ed6ba0100228464ce4d0
Dmitry Chekanov <chd@kodix.ru>
2023-05-30 14:40:45 +0300

    Homework-04 done

--- otus-04/src/otus_04/homework/scramblies.clj
+++ otus-04/src/otus_04/homework/scramblies.clj
@@ -3,8 +3,21 @@
 ;; Оригинальная задача:
 ;; https://www.codewars.com/kata/55c04b4cc56a697bb0000048
 
+
+(defn count-chars [chars]
+  (reduce (fn [acc c]
+            (update acc c (fnil inc 0)))
+          {}
+          chars))
+
+
 (defn scramble?
   "Функция возвращает true, если из букв в строке letters
   можно составить слово word."
   [letters word]
-  nil)
+  (let [letters-map (count-chars letters)
+        word-map    (count-chars word)]
+    (every? (fn [[k v]]
+              (<= v (letters-map k 0)))
+            word-map)))
+
