5df414b1986ee04d9959f3c8c4d01fc015529a2f
Dmitry Pavlov <mitrickx@mail.ru>
2023-07-02 16:15:39 +0300

    Homework8 processing files in parallel

--- otus-16/src/otus_16/homework.clj
+++ otus-16/src/otus_16/homework.clj
@@ -1,21 +1,83 @@
-(ns otus-16.homework)
+(ns otus-16.homework
+  (:gen-class)
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]))
 
+(defn parse-record
+  [record]
+  (if-let [[_ request bytes referrer] (re-find #"\"(.*?)\" \d+ (\d+) \"(.*?)\"" record)]
+    (let [[_ url-with-params _] (str/split (or request "") #" ")
+          [_ url] (re-find #"([^\?]*)" (or url-with-params ""))]
+      {:url (or url "") :bytes (or (parse-long bytes) 0) :referrer (or referrer "")})
+    {:url "" :bytes 0 :referrer ""}))
 
+(defn calc-total-bytes
+  [lines custom-filter]
+  (transduce
+    (comp
+      (map parse-record)
+      (filter custom-filter)
+      (map :bytes))
+    + lines))
 
-(defn solution [& {:keys [url referrer]
-                   :or   {url :all referrer :all}}]
-  (println "doing something")
-  {:total-bytes      12345
-   ;; если указан параметр url, то в хэш-мапе будет только одно значение
-   :bytes-by-url     {"some-url" 12345}
-   ;; если указан параметр referrer, то в хэш-мапе будет только одно значение
-   :urls-by-referrer {"some-referrer" 12345}})
+(defn process-file
+  [file custom-filter]
+  (let [lines (line-seq file)]
+    (future
+      (calc-total-bytes lines custom-filter))))
 
+(defn process-files
+  [files custom-filter]
+  (map #(process-file % custom-filter) files))
 
+(defn ->custom-filter
+  [{:keys [url referrer]}]
+  (cond
+    (and (some? url) (some? referrer))
+    (fn [record] (and (= (:url record) url)
+                      (= (:referrer record) referrer)))
 
-(comment
- ;; возможные вызовы функции
- (solution)
- (solution :url "some-url")
- (solution :referrer "some-referrer")
- (solution :url "some-url" :referrer "some-referrer"))
+    (some? url)
+    (fn [record] (= (:url record) url))
+
+    (some? referrer)
+    (fn [record] (= (:referrer record) referrer))
+
+    :default (constantly true)))
+
+(defn open-files
+  [file-paths]
+  (map io/reader file-paths))
+
+(defn close-files
+  [files]
+  (doseq [f files] (.close f)))
+
+(defmulti solution (fn [files & _] (type (first files))))
+
+(defmethod solution java.lang.String
+  [file-paths & rest]
+  (let [files (open-files file-paths)]
+    (try
+      (apply solution files rest)
+      (finally close-files files))))
+
+(defmethod solution java.io.Reader
+  [files & {:keys [url referrer]
+            :or {url nil referrer nil}}]
+  (let [custom-filter (->custom-filter {:url url :referrer referrer})]
+    (apply + (map deref (process-files files custom-filter)))))
+
+(defn get-file-paths
+  [file-prefix numbers]
+  (map #(str file-prefix %) numbers))
+
+(defn -main
+  [& [file-prefix]]
+  (if (nil? file-prefix)
+    (do (println "Pass file prefix")
+        (System/exit -1))
+    (do
+      (println "Total bytes of [log.{2-9}]"
+               (time (solution (get-file-paths file-prefix (range 2 10)))))
+      (shutdown-agents))))
--- otus-16/test/otus_16/homework_test.clj
+++ otus-16/test/otus_16/homework_test.clj
@@ -1,6 +1,45 @@
 (ns otus-16.homework-test
-  (:require [clojure.test :refer :all]
-            [otus-16.core :refer :all]))
+  (:require [clojure.string :as str]
+            [clojure.java.io :as io]
+            [clojure.test :refer :all]
+            [otus-16.homework :as sut]))
 
+(def test-input
+  (str/join "\n" ["176.193.24.191 - - [19/Jul/2020:07:41:32 +0000] \"GET /new/ HTTP/2.0\" 200 15716 \"https://baneks.site/\" \"Mozilla/5.0 (X11; Linux x86_64) AppleWeb\nKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36\" \"-\""
+                  "176.193.24.191 - - [19/Jul/2020:07:41:32 +0000] \"GET /new/?p=1 HTTP/2.0\" 200 15716 \"https://baneks.site/\" \"Mozilla/5.0 (X11; Linux x86_64) AppleWeb\nKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36\""
+                  "176.193.24.191 - - [19/Jul/2020:07:41:35 +0000] \"GET /admin/ HTTP/2.0\" 302 0 \"-\" \"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, li\nke Gecko) Chrome/81.0.4044.138 Safari/537.36\""
+                  "178.154.200.81 - - [19/Jul/2020:07:49:24 +0000] \"GET /-hello-my-name-is-Peter-without-s-but-there-is-no/ HTTP/1.1\" 200 11372 \"-\" \"Mozilla/5.0 (compatible; YandexBot/3.0; +http://yandex.com/bots)\""
+                  "178.154.200.81 - - [19/Jul/2020:07:49:24 +0000] \"GET /-hello-my-name-is-Peter-without-s-but-there-is-no/ HTTP/1.1\" 200 11372 \"-\" \"Mozilla/5.0 (compatible; YandexBot/3.0; +http://yandex.com/bots)\""
+                  "141.8.142.75 - - [20/Sep/2020:22:35:19 +0000] \"GET /%D0%BA%D0%BE%D1%80%D1%80%D0%B5%D1%81%D0%BF%D0%BE%D0%BD%D0%B4%D0%B5%D0%BD%D1%82-%D0%B2%D0%B5%D0%B4%D0%B5%D1%82-%D1%80%D0%B5%D0%BF%D0%BE%D1%80%D1%82%D0%B0%D0%B6-%D1%81-%D0%9A%D1%80%D0%B0%D1%81%D0%BD%D0%BE%D0%B9-%D0%BF%D0%BB%D0%BE%D1%89%D0%B0%D0%B4%D0%B8/?p=2 HTTP/1.1\" 200 9530 \"-\" \"Mozilla/5.0 (compatible; YandexBot/3.0; +http://yandex.com/bots)\""]))
 
+(deftest test-solution
+  (testing "without filters"
+    (with-open [r (io/reader (char-array test-input))]
+      (is (= (sut/solution [r]) 63706))))
+
+  (testing "with-url-filter"
+    (with-open [r (io/reader (char-array test-input))]
+      (is (= (sut/solution [r] :url "/new/") 31432))))
+
+  (testing "with-url-filter-and-not-exiting"
+    (with-open [r (io/reader (char-array test-input))]
+      (is (= (sut/solution [r] :url "/not-existing-url/") 0))))
+
+  (testing "with-referrer-filter"
+    (with-open [r (io/reader (char-array test-input))]
+      (is (= (sut/solution [r] :referrer "-") 32274))))
+
+  (testing "with-referrer-filter-and-not-existing"
+    (with-open [r (io/reader (char-array test-input))]
+      (is (= (sut/solution [r] :referrer "not-existing-referrer") 0))))
+
+  (testing "with-url-and-referrer-filter"
+    (with-open [r (io/reader (char-array test-input))]
+      (is (= (sut/solution [r] :url "/-hello-my-name-is-Peter-without-s-but-there-is-no/"
+                           :referrer "-") 22744))))
+
+  (testing "with-url-and-referrer-filter-not-existing"
+    (with-open [r (io/reader (char-array test-input))]
+      (is (= (sut/solution [r] :url "unknown-url"
+                           :referrer "unknown-referrer") 0)))))
 
